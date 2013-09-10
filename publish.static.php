6961365012cf2e80183dd234549f921db60545b5
vinceMeens <v.w.a.meens@comcordis.com>
2013-09-11 01:20:30 +0200

    Static publishing per project

---
+++ publish.static.php
@@ -0,0 +1,26 @@
+<?php
+
+XXX_Static_Publisher::publishMergedFilesFromOtherProject($project, $deployIdentifier, array
+(
+	'XXX_Array.js',
+	'XXX_Default.js',
+	'XXX_Type.js',
+	'XXX_Number.js',
+	'XXX_String.js',
+	'XXX_String_Base64.js',
+	'XXX_String_ContextBoundaries.js',
+	'XXX_String_HTMLEntities.js',
+	'XXX_String_JSON.js',
+	'XXX_String_Levenshtein.js',
+	'XXX_String_Pattern.js',
+	'XXX_String_RichTextFormatting.js',
+	'XXX_String_Unicode.js',
+	'XXX_String_Search.js',
+	'XXX_Search_SimpleIndex.js',
+	'XXX_Search_InvertedIndex.js',
+	'XXX_Timestamp.js',
+	'XXX_TimestampHelpers.js'
+),
+'initialize.js');
+
+?>
